193ef47d0b14ac0019a35df5850b6aca09185801
rafaellamiras <rafaellamiras@gmail.com>
2026-05-13 17:02:28 -0300

    Projeto PetShop - desafios para melhorias.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // scripts petshop
+document.getElementById("formContato").addEventListener("submit", function (e) {
+  //se nào possui a função "onclick" no button, precisa escutar um evento (submit)
+  e.preventDefault();
+
+  let nome = document.getElementById("Nome").value.trim();
+  let email = document.getElementById("Email").value.trim();
+  let mensagem = document.getElementById("Mensagem").value.trim();
+
+  if (!nome == "" || email == "" || !mensagem == "") {
+    alert("Preencha todos os campos!");
+    return;
+  }
+  this.reset();
+});
+
+// **************************** //
+
+let botaoMenu = document.getElementById("menu-btn");
+let menu = document.getElementById("menu");
+
+botaoMenu.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+});
